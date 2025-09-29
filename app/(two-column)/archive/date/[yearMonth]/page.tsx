259a10e76a7e9d9dcbba24c277066b5ce87111f6
mujara <mujyara_mujyara@hotmail.co.jp>
2025-09-29 15:40:59 +0900

    デプロイエラー修正48

--- app/(two-column)/archive/date/[yearMonth]/page.tsx
+++ app/(two-column)/archive/date/[yearMonth]/page.tsx
@@ -1,3 +1,4 @@
+import { ParsedUrlQuery } from "querystring";
 import PageTitle from "@/app/_components/PageTitle";
 import Topicpath from "@/app/_components/Topicpath";
 import { getArchiveList } from "@/app/_libs/microcms";
@@ -5,26 +6,37 @@ import ArchiveList from "@/app/_components/ArchiveList";
 import Pagination from "@/app/_components/Pagination";
 import { ARCHIVE_LIST_LIMIT } from "@/app/_constants";
 
-export default async function Page({
-  params,
-  searchParams,
-}: {
-  params: { yearMonth: string };
-  searchParams?: { page?: string };
-}) {
+// URL パラメータの型
+interface PageParams extends ParsedUrlQuery {
+  yearMonth: string;
+}
+
+// searchParams の型
+interface PageSearchParams {
+  page?: string;
+}
+
+// ページ関数の Props 型
+interface Props {
+  params: PageParams;
+  searchParams?: PageSearchParams;
+}
+
+export default async function Page({ params, searchParams }: Props) {
   const { yearMonth } = params;
   const currentPage = Number(searchParams?.page ?? "1");
 
-  // --- YYYY-MM を分解 ---
+  // YYYY-MM を分解
   const [year, month] = yearMonth.split("-");
 
-  // --- microCMS で月別の記事を取得する ---
+  // 月初と月末の ISO 文字列を作成
   const startDate = `${year}-${month}-01T00:00:00Z`;
   const endDate = new Date(Number(year), Number(month), 0); // 月末日
   const endDateStr = `${endDate.getFullYear()}-${String(
     endDate.getMonth() + 1
   ).padStart(2, "0")}-${String(endDate.getDate()).padStart(2, "0")}T23:59:59Z`;
 
+  // microCMS から記事取得
   const { contents: archive, totalCount } = await getArchiveList({
     limit: ARCHIVE_LIST_LIMIT,
     offset: (currentPage - 1) * ARCHIVE_LIST_LIMIT,
