85bb85441c0f362372376089fa10e69e8057c9ca
mujara <mujyara_mujyara@hotmail.co.jp>
2025-11-29 12:25:26 +0900

    一時的に削除4

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,14 @@ import Image from "next/image";
   /* import styles from "./page.module.css";*/
 }
 
-import HomeArchiveBox from "@/app/_components/HomeArchiveBox";
+import HomeHeader from "@/app/_components/HomeHeader";
 import HomeAboutBox from "@/app/_components/HomeAboutBox";
-import { getHomeArchiveList } from "@/app/_libs/microcms";
-import { HOME_ARCHIVE_LIMIT } from "@/app/_constants";
 import Footer from "./_components/Footer";
 import ButtonRise from "@/app/_components/ButtonRise";
 
 export const revalidate = 60;
 
 export default async function Home() {
-  const data = await getHomeArchiveList({ limit: HOME_ARCHIVE_LIMIT });
   return (
     <body className="home">
       <div id="wrapper">
@@ -32,8 +29,9 @@ export default async function Home() {
           </figure>
         </div>
 
+        <HomeHeader />
+
         <div id="contents" className="clearfix">
-          <HomeArchiveBox data={data} />
           <HomeAboutBox />
         </div>
         {/* /#contents */}
