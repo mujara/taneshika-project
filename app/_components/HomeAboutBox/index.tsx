3d77527062a62c8661d4e04c8dd56f448012a077
mujara <mujyara_mujyara@hotmail.co.jp>
2025-11-29 13:49:48 +0900

    homeAboutBox__contentの削除

--- app/_components/HomeAboutBox/index.tsx
+++ app/_components/HomeAboutBox/index.tsx
@@ -81,51 +81,192 @@ export default function HomeAboutBox() {
           </figure>
           {/* /.homeAboutBox__bg */}
         </div>
-        <div className="homeAboutBox__content">
-          <div className="homeAboutBox__title">
-            <h2>What is this</h2>
-            {/* /.homeAboutBox__title */}
+        <div className="homeAboutBox__image">
+          <figure className="parallax--type06">
+            <span>
+              <Image
+                src="/img/home/imgRope01.png"
+                alt=""
+                fill
+                unoptimized={process.env.NODE_ENV === "development"}
+              />
+            </span>
+          </figure>
+          <figure className="parallax--type07">
+            <span>
+              <Image
+                src="/img/home/imgRope02.png"
+                alt=""
+                fill
+                unoptimized={process.env.NODE_ENV === "development"}
+              />
+            </span>
+          </figure>
+          <div className="homeAboutBox__playingCard">
+            <span>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_h04.png"
+                  alt="ハートの4"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_back.png"
+                  alt="トランプの裏側"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
+            </span>
+            {/* /.homeAboutBox__playingCard */}
           </div>
-          <p>
-            <span className="homeAboutBox__sentence01">
-              ヤッホー！当ページ<small>に</small>
-              <br />
-              ご訪問<small>いただき</small>、ありがとうございます。
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard02.png"
+              alt="ハートのエース"
+              width={197}
+              height={384}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard03.png"
+              alt="クローバーの10"
+              width={234}
+              height={302}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <div className="homeAboutBox__playingCard">
+            <span>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_d07.png"
+                  alt="ダイヤの7"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_back.png"
+                  alt="トランプの裏側"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
             </span>
-            <span className="homeAboutBox__sentence02">
-              感謝<small>感</small>
-              <small>激</small>雨<small>あら</small>れ！
+            {/* /.homeAboutBox__playingCard */}
+          </div>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard05.png"
+              alt="トランプの裏側"
+              width={262}
+              height={395}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard06.png"
+              alt="クローバーのキング"
+              width={340}
+              height={392}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard07.png"
+              alt="クローバーのクイーン"
+              width={209}
+              height={268}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <div className="homeAboutBox__playingCard">
+            <span>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_d04.png"
+                  alt="ダイヤの4"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_back.png"
+                  alt="トランプの裏側"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>{" "}
             </span>
-            <span className="homeAboutBox__sentence03">
-              当サイト<small>は</small>ご機嫌<small>な</small>
-              <span className="name__ara">
-                <span>十</span>
-                <span>十</span>
-                <span>亡</span>
-                <span>川</span>
-              </span>
-              <span className="name__kawa">
-                <span>一</span>
-                <span>一</span>
-                <span>一</span>
-              </span>
-              <small>が</small>備忘<small>や</small>
-              <br className="hidden-sp" />
-              アウトプット<small>的なかんじを</small>目的に
-              <br className="hidden-sp" />
-              <span className="homeAboutBox__sentence04">
-                <small>利用して</small>います。
-              </span>
+            {/* /.homeAboutBox__playingCard */}
+          </div>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard09.png"
+              alt="クローバーの3"
+              width={217}
+              height={258}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard10.png"
+              alt="クローバーの9"
+              width={198}
+              height={243}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <figure>
+            <Image
+              src="/img/home/imgPlayingCard11.png"
+              alt="ジョーカー"
+              width={171}
+              height={318}
+              unoptimized={process.env.NODE_ENV === "development"}
+            />
+          </figure>
+          <div className="homeAboutBox__playingCard">
+            <span>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_hq.png"
+                  alt="ダイヤの4"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>
+              <figure>
+                <Image
+                  src="/img/home/imgPlayingCard_back.png"
+                  alt="トランプの裏側"
+                  width={52}
+                  height={70.4}
+                  unoptimized={process.env.NODE_ENV === "development"}
+                />
+              </figure>{" "}
             </span>
-          </p>
-          <p>
-            Thank you for visiting.
-            <br />
-            This site serves as a memo pad, an output platform, <br />
-            and a storage repository for parts related to AraRiver&apos;s
-            creations.
-          </p>
-          {/* /.homeAboutBox__content */}
+            {/* /.homeAboutBox__playingCard */}
+          </div>
+          {/* /.homeAboutBox__image */}
         </div>
         <div className={cx("homeAboutBox__portrait", "parallax--type05")}>
           <figure>
